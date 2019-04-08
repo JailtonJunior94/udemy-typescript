6681449655db2ef3fbd340a46313a926e93915c1
Jailton Junior <jailton.junior@partnergroup.com.br>
2019-04-08 09:43:35 -0300

    let & const

---
+++ 04-novos-recursos/ecmascript.ts
@@ -0,0 +1,31 @@
+// let & const
+let seraQuepode = '?'
+console.log(seraQuepode)
+
+let estaFrio = true
+if (estaFrio) {
+    let acao = 'Colocar o casado!'
+    console.log(acao)
+}
+
+const cpf: string = '430.406.068-67'
+// cpf = '999.999.999=99'
+console.log(cpf)
+
+var segredo = 'externo'
+function revelar() {
+    var segredo = 'interno'
+    console.log(segredo)
+}
+revelar()
+console.log(segredo)
+
+{
+    const def = 'def'
+    console.log(def)
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+// console.log(i)
