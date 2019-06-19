d76c6e2c56fe234a998cbeb66b14a836687e0be6
Jailton Junior <jailton.junior94@outlook.com>
2019-06-19 07:55:22 -0300

    generics

--- 08-generics/generics.js
+++ 08-generics/generics.js
@@ -1,1 +1,41 @@
 "use strict";
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo('João'));
+console.log(echo(27));
+console.log(echo({ nome: 'João', idade: 27 }));
+// Usando Generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('João'));
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: 'João', idade: 27 }));
+// Usando Generics Array
+const avaliacoes = [10, 9.3, 7, 7];
+avaliacoes.push(8.4);
+console.log(avaliacoes);
+// Array
+function imprimir(args) {
+    args.forEach(element => console.log(element));
+}
+imprimir([1, 2, 3]);
+imprimir([1, 2, 3]);
+imprimir(['Ana', 'Bia']);
+imprimir([{ nome: 'Fulano', idade: 22 }]);
+const chamarEcho = echoMelhorado;
+console.log(chamarEcho('Alguma coisa'));
+// Classe com Generics
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new OperacaoBinaria('Bom', 'dia').executar());
+console.log(new OperacaoBinaria(3, 7).executar());
+console.log(new OperacaoBinaria(3, 'Opa').executar());
--- 08-generics/generics.ts
+++ 08-generics/generics.ts
@@ -16,3 +16,35 @@ console.log(echoMelhorado<number>(27))
 console.log(echoMelhorado({ nome: 'João', idade: 27 }))
 
 // Usando Generics Array
+const avaliacoes: Array<number> = [10, 9.3, 7, 7]
+avaliacoes.push(8.4)
+console.log(avaliacoes)
+
+// Array
+function imprimir<T>(args: T[]) {
+    args.forEach(element => console.log(element))
+}
+
+imprimir([1, 2, 3]);
+imprimir<number>([1, 2, 3]);
+imprimir<string>(['Ana', 'Bia'])
+type Aluno = { nome: string, idade: number }
+imprimir<Aluno>([{ nome: 'Fulano', idade: 22 }])
+
+// Tipo Genérico
+type Echo = <T>(data: T) => T
+const chamarEcho: Echo = echoMelhorado
+console.log(chamarEcho<string>('Alguma coisa'))
+
+// Classe com Generics
+class OperacaoBinaria<T> {
+    constructor(public operando1: any, public operando2: any) { }
+
+    executar() {
+        return this.operando1 + this.operando2
+    }
+}
+
+console.log(new OperacaoBinaria('Bom', 'dia').executar())
+console.log(new OperacaoBinaria(3, 7).executar())
+console.log(new OperacaoBinaria(3, 'Opa').executar())
