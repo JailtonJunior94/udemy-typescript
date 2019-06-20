e36f785dc5b841edc177a105d05529d5087bc700
Jailton Júnior <jailton.junior94@outlook.com>
2019-06-20 17:04:23 -0300

    propriedades statics

--- 05-classes-objetos/classes.js
+++ 05-classes-objetos/classes.js
@@ -17,3 +17,72 @@ class DataEsperta {
 }
 const aniversarioEsperto = new DataEsperta(6, 1, 1994);
 const casamentoEsperto = new DataEsperta;
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+    resumo() {
+        return `${this.nome} custa R$ ${this.preco} (${this.desconto * 100}% OFF)`;
+    }
+}
+const prod1 = new Produto('Caneta Bic Preta', 4.20, 0.06);
+console.log(prod1.resumo());
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'KA', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+class Ferrari extends Carro {
+    constructor(modelo, velocidadeMaxima) {
+        super('Ferrari', modelo, velocidadeMaxima);
+    }
+    acelerar() {
+        return super.alterarVelocidade(20);
+    }
+    frear() {
+        return super.alterarVelocidade(-15);
+    }
+}
+const f40 = new Ferrari('F40', 325);
+console.log(f40.acelerar());
+console.log(f40.frear());
+class Pessoa {
+    constructor() {
+        this._idade = 0;
+    }
+    get idade() {
+        return this._idade;
+    }
+    set idade(valor) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor;
+        }
+    }
+}
+const pessoa1 = new Pessoa;
+pessoa1.idade = 10;
+console.log(pessoa1.idade);
--- 05-classes-objetos/classes.ts
+++ 05-classes-objetos/classes.ts
@@ -31,3 +31,69 @@ class Produto {
 
 const prod1 = new Produto('Caneta Bic Preta', 4.20, 0.06)
 console.log(prod1.resumo())
+
+class Carro {
+    private velocidadeAtual: number = 0
+
+    constructor(public marca: string, public modelo: string, private velocidadeMaxima: number = 200) { }
+
+    protected alterarVelocidade(delta: number): number {
+        const novaVelocidade = this.velocidadeAtual + delta
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade
+        } else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+
+        return this.velocidadeAtual
+    }
+
+    public acelerar(): number {
+        return this.alterarVelocidade(5)
+    }
+
+    public frear(): number {
+        return this.alterarVelocidade(-5)
+    }
+}
+
+const carro1 = new Carro('Ford', 'KA', 185)
+Array(50).fill(0).forEach(() => carro1.acelerar())
+
+class Ferrari extends Carro {
+    constructor(modelo: string, velocidadeMaxima: number) {
+        super('Ferrari', modelo, velocidadeMaxima)
+    }
+
+    public acelerar(): number {
+        return super.alterarVelocidade(20)
+    }
+
+    public frear(): number {
+        return super.alterarVelocidade(-15)
+    }
+}
+
+const f40 = new Ferrari('F40', 325)
+console.log(f40.acelerar())
+console.log(f40.frear())
+
+class Pessoa {
+    private _idade: number = 0
+
+    get idade(): number {
+        return this._idade;
+    }
+
+    set idade(valor: number) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor
+        }
+    }
+}
+
+const pessoa1 = new Pessoa
+pessoa1.idade = 10
+console.log(pessoa1.idade)
