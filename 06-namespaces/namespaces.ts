16b81a4f0128463413da74db365e2bbb8d3c1fe3
Jailton Júnior <jailton.junior94@outlook.com>
2019-07-16 19:30:28 -0300

    conclusão do modulo (namespaces e modulos)

---
+++ 06-namespaces/namespaces.ts
@@ -0,0 +1,9 @@
+///<reference path="geometricaCirc.ts"/>
+///<reference path="geometricaRect.ts"/>
+
+// const PI = 2.99
+
+console.log(Geometria.Area.circunferencia(10))
+console.log(Geometria.Area.retangulo(10, 20))
+
+// console.log(PI)
