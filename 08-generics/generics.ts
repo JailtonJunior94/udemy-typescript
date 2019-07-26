ac6980cace3ac4f53a3cbee24976191f9887310a
Jailton Júnior <jailton.junior94@outlook.com>
2019-07-25 21:37:01 -0300

    conclusão do módulo generics

--- 08-generics/generics.ts
+++ 08-generics/generics.ts
@@ -55,3 +55,100 @@ class SomaBinaria extends OperacaoBinaria<number, number> {
 }
 
 console.log(new SomaBinaria(3, 4).executar())
+
+class DiferencaEntreDatas extends OperacaoBinaria<Data, string> {
+    getTime(data: Data): number {
+        let { dia, mes, ano } = data
+        return new Date(`${mes}/${dia}/${ano}`).getTime()
+    }
+
+    executar(): string {
+        const t1 = this.getTime(this.operando1)
+        const t2 = this.getTime(this.operando2)
+
+        const diferenca = Math.abs(t1 - t2)
+        const dia = 1000 * 60 * 60 * 24
+
+        return `${Math.ceil(diferenca / dia)} dia(s)`
+    }
+}
+
+const d1 = new Data(1, 2, 2020)
+const d2 = new Data(5, 2, 2020)
+console.log(new DiferencaEntreDatas(d1, d2).executar())
+
+class Fila<T extends number | string> {
+    private fila: Array<T>
+
+    constructor(...args: T[]) {
+        this.fila = args
+    }
+
+    entrar(elemento: T) {
+        this.fila.push(elemento)
+    }
+
+    proximo(): T | null {
+        if (this.fila.length >= 0 && this.fila[0]) {
+            const primeiro = this.fila[0]
+            this.fila.splice(1, 1)
+
+            return primeiro
+        } else {
+            return null
+        }
+    }
+
+    imprimir() {
+        console.log(this.fila)
+    }
+}
+
+const fila = new Fila<string>('Gui', 'Pedro', 'Ana', 'Lu')
+fila.imprimir()
+fila.entrar('Rafael')
+fila.imprimir()
+console.log(fila.proximo())
+console.log(fila.proximo())
+
+const novaFila = new Fila<number>(1, 2, 3)
+novaFila.imprimir()
+
+type Par<C, V> = { chave: C, valor: V }
+
+class Mapa<C, V> {
+    itens: Array<Par<C, V>> = new Array<Par<C, V>>()
+
+    colocar(par: Par<C, V>) {
+        const encontrado = this.obter(par.chave)
+        if (encontrado) {
+            encontrado.valor = par.valor
+        } else {
+            this.itens.push(par)
+        }
+    }
+
+    obter(chave: C): Par<C, V> | null {
+        const resultado = this.itens.filter(i => i.chave === chave)
+        return resultado ? resultado[0] : null
+    }
+
+    limpar() {
+        this.itens = new Array<Par<C, V>>()
+    }
+
+    imprimir() {
+        console.log(this.itens)
+    }
+}
+
+const mapa = new Mapa<number, string>()
+mapa.colocar({ chave: 1, valor: 'Pedro' })
+mapa.colocar({ chave: 2, valor: 'Rebeca' })
+mapa.colocar({ chave: 3, valor: 'Maria' })
+mapa.colocar({ chave: 1, valor: 'Gustavo' })
+
+console.log(mapa.obter(2))
+mapa.imprimir()
+mapa.limpar()
+mapa.imprimir()
